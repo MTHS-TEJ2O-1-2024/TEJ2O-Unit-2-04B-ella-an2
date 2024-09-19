c568154193ad509fb01c49a073611be8ef639021
ella-an2 <ella.an@mths.ca>
2024-09-19 11:20:38 -0400

    Changed the comment and programmed it to get the temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ella
+ * Created on: Sep 2024
+ * This program tells the current temperature
 */
 
-basic.showString('Hello, World!')
+//variable for the current temperature
+let currentTemperature = input.temperature()
+
+//
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+input.onButtonPressed(Button.A, function() {
+    currentTemperature = input.temperature()
+    basic.showString('The current temperature is:' + (currentTemperature).toString() + '°C')
+})
