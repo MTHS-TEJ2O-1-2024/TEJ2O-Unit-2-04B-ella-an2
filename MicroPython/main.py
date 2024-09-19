abdaf94e413434600bcd40fb7f0f085d96685771
ella-an2 <ella.an@mths.ca>
2024-09-19 19:46:18 +0000

    Finished the program to show the temperature

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,8 +5,16 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
-from time import
+from time import *
 
-current_teperature = temperature
+current_temperature = temperature
 
 
+display.clear
+display.show(Image.HAPPY)
+sleep(1)
+
+while True:
+    if button_a.is_pressed():
+        current_temperature = temperature
+        display.scroll("The current temperature is:" + str(current_temperature) + "°C")
